21fe46010dcbb447f4da7904912dc9f91a18765c
Luke Wendling <luke@lukewendling.com>
2017-03-03 08:05:21 -0600

    update tests. add inq func

--- loogo/main.go
+++ loogo/main.go
@@ -7,10 +7,10 @@ import (
 	"strings"
 )
 
-// QueryParams is a slice of QueryParam
+// QueryParams is a slice of QueryParam.
 type QueryParams []QueryParam
 
-// QueryParam is a field-value pair
+// QueryParam is a field-value pair.
 type QueryParam struct {
 	QueryType string
 	Field     string
@@ -29,6 +29,8 @@ func BuildQuery(params QueryParams, countOnly bool) string {
 			qs = append(qs, Between(p, countOnly))
 		case "eq":
 			qs = append(qs, Eq(p, countOnly))
+		case "inq":
+			qs = append(qs, Inq(p, countOnly))
 		default:
 			fmt.Println("unknown QueryType")
 		}
@@ -37,7 +39,7 @@ func BuildQuery(params QueryParams, countOnly bool) string {
 	return "?" + strings.Join(qs, "&")
 }
 
-// Between returns loopback querystring for between queries
+// Between returns loopback querystring for between queries.
 func Between(p QueryParam, countOnly bool) string {
 	qs := []string{}
 	prefix := "filter"
@@ -54,7 +56,7 @@ func Between(p QueryParam, countOnly bool) string {
 	return strings.Join(parts, "")
 }
 
-// Eq returns loopback querystring for equality queries
+// Eq returns loopback querystring for equality queries.
 func Eq(p QueryParam, countOnly bool) string {
 	prefix := "filter"
 	if countOnly {
@@ -64,7 +66,25 @@ func Eq(p QueryParam, countOnly bool) string {
 	return fmt.Sprintf("%s[where][%s]=%s", prefix, p.Field, p.Values[0])
 }
 
-// Pager pages over results from URL.
+// Inq returns loopback querystring for inclusion queries.
+// ex. filter[where][name][inq]=foo&filter[where][name][inq]=bar
+func Inq(p QueryParam, countOnly bool) string {
+	qs := []string{}
+	prefix := "filter"
+	if countOnly {
+		prefix = ""
+	}
+
+	for _, v := range p.Values {
+		qs = append(qs, fmt.Sprintf("[where][%s][inq]=%s", p.Field, v))
+	}
+
+	parts := []string{prefix, strings.Join(qs, "&"+prefix)}
+
+	return strings.Join(parts, "")
+}
+
+// Pager pages over results from URL fetch.
 // Uses a scrolling technique, not offsets (offsets are slow in mongo).
 type Pager struct {
 	URL         string
@@ -74,19 +94,29 @@ type Pager struct {
 	PageSize    int
 	ScrollID    string
 	OrderBy     string
+	Query       string
 }
 
-// NewPager inits a Pager instance
-func NewPager(URL string, params QueryParams, pageSize int) *Pager {
+// NewPager inits a Pager instance.
+func NewPager(URL string, params QueryParams, pageSize int) (*Pager, error) {
 	URL = strings.TrimRight(URL, "/")
-	orderBy := "_id ASC"
+	orderBy := "_id"
 
-	countEndpoint := URL + BuildQuery(params, true)
-	tc := getCount(countEndpoint)
+	// 'findOne', 'count' do not use 'filter' prefix
+	qs := BuildQuery(params, true)
 
-	findOneEndpoint := URL + "/findone" + BuildQuery(params, false) + fmt.Sprintf("&filter[order]=%s", orderBy)
+	countEndpoint := URL + "/count" + qs
+	tc, err := getCount(countEndpoint)
+	if err != nil {
+		return nil, err
+	}
 
-	scrollID := getFirstID(findOneEndpoint)
+	//find first item matching query, sorted
+	findOneEndpoint := URL + "/findone" + qs + fmt.Sprintf("&filter[order]=%s", orderBy)
+	scrollID, err := getFirstID(findOneEndpoint)
+	if err != nil {
+		return nil, err
+	}
 
 	return &Pager{
 		URL:        URL,
@@ -95,13 +125,14 @@ func NewPager(URL string, params QueryParams, pageSize int) *Pager {
 		PageSize:   pageSize,
 		OrderBy:    orderBy,
 		ScrollID:   scrollID,
-	}
+		Query:      BuildQuery(params, false),
+	}, nil
 }
 
-func getCount(countEndpoint string) int {
+func getCount(countEndpoint string) (int, error) {
 	resp, err := http.Get(countEndpoint)
 	if err != nil {
-		panic(err)
+		return -1, err
 	}
 
 	defer resp.Body.Close()
@@ -110,13 +141,13 @@ func getCount(countEndpoint string) int {
 
 	json.NewDecoder(resp.Body).Decode(&results)
 
-	return int(results["count"])
+	return int(results["count"]), nil
 }
 
-func getFirstID(findOneEndpoint string) string {
+func getFirstID(findOneEndpoint string) (string, error) {
 	resp, err := http.Get(findOneEndpoint)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	defer resp.Body.Close()
@@ -125,11 +156,11 @@ func getFirstID(findOneEndpoint string) string {
 
 	json.NewDecoder(resp.Body).Decode(&result)
 
-	return result["id"].(string)
+	return result["id"].(string), nil
 }
 
-// ByPage adds scrolling filters
-func (p *Pager) ByPage() string {
+// byPage adds scrolling filters.
+func (p *Pager) byPage() string {
 	op := "gte"
 	if p.CurrentPage > 0 {
 		op = "gt"
@@ -138,23 +169,23 @@ func (p *Pager) ByPage() string {
 	return fmt.Sprintf("filter[where][id][%s]=%s&filter[limit]=%d&filter[order]=%s", op, p.ScrollID, p.PageSize, p.OrderBy)
 }
 
-// Results are query results
+// Results are query results.
 type Results []Result
 
-// Result is a single item
+// Result is a single item.
 type Result map[string]interface{}
 
-// GetNext returns next page of results, nil if past upper bounds
-func (p *Pager) GetNext() Results {
+// GetNext returns next set of results, nil if past upper bounds, or any error.
+func (p *Pager) GetNext() (Results, error) {
 	if p.TotalCount == 0 || (p.CurrentPage >= p.TotalPages) {
-		return nil
+		return nil, nil
 	}
 
-	url := strings.Join([]string{p.URL, p.ByPage()}, "&")
+	url := strings.Join([]string{p.URL + p.Query, p.byPage()}, "&")
 
 	resp, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	defer resp.Body.Close()
@@ -162,15 +193,15 @@ func (p *Pager) GetNext() Results {
 	results := Results{}
 
 	json.NewDecoder(resp.Body).Decode(&results)
-	fmt.Println(len(results))
+	// fmt.Println(len(results))
 
 	if len(results) != 0 {
-		// get last item in batch for cursor marker
+		// get last item in batch to update scroll id
 		p.ScrollID = results[len(results)-1]["id"].(string)
 	} else {
-		return nil
+		return results, nil
 	}
 
 	p.CurrentPage++
-	return results
+	return results, nil
 }
--- loogo/main_test.go
+++ loogo/main_test.go
@@ -1,98 +1,106 @@
 package loogo
 
-import "testing"
+import (
+	_ "fmt"
+	_ "reflect"
+	"testing"
+)
 
-func Test_Between(t *testing.T) {
-	tests := []struct {
-		name  string
-		param QueryParam
-		want  string
-	}{
-		{
-			"between",
-			QueryParam{
-				QueryType: "Between",
-				Field:     "created",
-				Values:    []string{"0", "1"},
-			},
-			"filter[where][created][between][0]=0&filter[where][created][between][1]=1",
-		},
-		{
-			"between",
-			QueryParam{
-				QueryType: "Between",
-				Field:     "created",
-				Values:    []string{"0", "1"},
-			},
-			"[where][created][between][0]=0&[where][created][between][1]=1",
-		},
+func TestInq(t *testing.T) {
+	p1 := QueryParam{
+		QueryType: "Inq",
+		Field:     "status",
+		Values:    []string{"done", "new"},
 	}
-	for _, tt := range tests {
-		if got := Between(tt.param, false); got != tt.want {
-			t.Errorf("%q. Between() = %v, want %v", tt.name, got, tt.want)
-		}
+
+	want := "filter[where][status][inq]=done&filter[where][status][inq]=new"
+	if got := Inq(p1, false); got != want {
+		t.Errorf("Inq() = %v, want %v", got, want)
 	}
 }
 
-func Test_Eq(t *testing.T) {
-	tests := []struct {
-		name  string
-		param QueryParam
-		want  string
-	}{
-		{
-			"eq",
-			QueryParam{
-				QueryType: "Eq",
-				Field:     "featurizer",
-				Values:    []string{"image"},
-			},
-			"filter[where][featurizer]=image",
-		},
-		{
-			"eq",
-			QueryParam{
-				QueryType: "Eq",
-				Field:     "featurizer",
-				Values:    []string{"image"},
-			},
-			"[where][featurizer]=image",
-		},
-	}
-	for _, tt := range tests {
-		if got := Eq(tt.param, false); got != tt.want {
-			t.Errorf("%q. Eq() = %v, want %v", tt.name, got, tt.want)
-		}
+func TestEq(t *testing.T) {
+	p1 := QueryParam{
+		QueryType: "Eq",
+		Field:     "status",
+		Values:    []string{"new"},
 	}
 
+	want := "filter[where][status]=new"
+	if got := Eq(p1, false); got != want {
+		t.Errorf("Eq() = %v, want %v", got, want)
+	}
 }
 
-func Test_BuildQuery(t *testing.T) {
-	tests := []struct {
-		name   string
-		params []QueryParam
-		want   string
-	}{
-		{
-			"buildquery",
-			[]QueryParam{
-				{
-					QueryType: "Eq",
-					Field:     "featurizer",
-					Values:    []string{"image"},
-				},
-				{
-					QueryType: "Eq",
-					Field:     "status",
-					Values:    []string{"new"},
-				},
-			},
-			"?filter[where][featurizer]=image&filter[where][status]=new",
-		},
+func TestBetween(t *testing.T) {
+	p1 := QueryParam{
+		QueryType: "Between",
+		Field:     "status",
+		Values:    []string{"1", "2"},
 	}
-	for _, tt := range tests {
-		if got := BuildQuery(tt.params, false); got != tt.want {
-			t.Errorf("%q. BuildQuery() = %v, want %v", tt.name, got, tt.want)
-		}
+
+	want := "filter[where][status][between][0]=1&filter[where][status][between][1]=2"
+	if got := Between(p1, false); got != want {
+		t.Errorf("Between() = %v, want %v", got, want)
 	}
 }
+
+func TestBuildQuery_not_for_count(t *testing.T) {
+	p1 := QueryParam{
+		QueryType: "Eq",
+		Field:     "status",
+		Values:    []string{"done"},
+	}
+
+	p2 := QueryParam{
+		QueryType: "Between",
+		Field:     "timestamp_ms",
+		Values:    []string{"1", "2"},
+	}
+
+	params := QueryParams{
+		p1,
+		p2,
+	}
+
+	want := "?filter[where][status]=done&filter[where][timestamp_ms][between][0]=1&filter[where][timestamp_ms][between][1]=2"
+	if got := BuildQuery(params, false); got != want {
+		t.Errorf("BuildQuery() = %v, want %v", got, want)
+	}
+}
+
+func TestBuildQuery_for_count(t *testing.T) {
+	p1 := QueryParam{
+		QueryType: "Eq",
+		Field:     "status",
+		Values:    []string{"new", "done"},
+	}
+
+	params := QueryParams{
+		p1,
+	}
+
+	want := "?[where][status]=new"
+	if got := BuildQuery(params, true); got != want {
+		t.Errorf("BuildQuery() = %v, want %v", got, want)
+	}
+}
+
+// func TestNewPager(t *testing.T) {
+// 	url := "http://localhost:3000/api/socialmediaposts/"
+// 	p1 := QueryParam{
+// 		QueryType: "Eq",
+// 		Field:     "featurizer",
+// 		Values:    []string{"df"},
+// 	}
+
+// 	params := QueryParams{
+// 		p1,
+// 	}
+
+// 	pager, _ := NewPager(url, params, 10)
+// 	res, _ := pager.GetNext()
+// 	fmt.Println(len(res), pager)
+// 	res, _ = pager.GetNext()
+// 	fmt.Println(len(res), pager)
+// }
