3498177a2e7f49d6b7a99a6596a252889c6908a6
Luke Wendling <luke@lukewendling.com>
2017-03-10 11:36:30 -0600

    add test

--- loogo/http_request_test.go
+++ loogo/http_request_test.go
@@ -25,3 +25,24 @@ func TestNewRequest(t *testing.T) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
+
+func TestNewRequest_custom_struct(t *testing.T) {
+	type MyObj struct {
+		Name   string   `json:"name"`
+		Things []string `json:"things"`
+	}
+	doc := MyObj{}
+	url := "http://bogus"
+	r := HTTPRequestParser{
+		client: &TestHTTPClient{},
+	}
+	err := r.NewRequest(NewRequestParams{URL: url}, &doc)
+	if err != nil {
+		t.Error(err)
+	}
+	got := doc.Name
+	want := "riot"
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
