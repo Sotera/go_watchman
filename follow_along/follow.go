9070bc78f2139a21de9af2961b196977a8568a72
Luke Wendling <luke@lukewendling.com>
2017-03-28 21:42:40 -0500

    page thru

--- follow_along/follow.go
+++ follow_along/follow.go
@@ -22,35 +22,56 @@ func main() {
 		panic(errors.New("missing required args"))
 	}
 
-	found, err := isFollowing(*follower, *followee)
+	found, err := isFollowing(*follower, *followee, fmt.Sprintf("https://mobile.twitter.com/%s/following", *follower))
+
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
 	fmt.Println(found)
 }
 
-func isFollowing(follower, followee string) (bool, error) {
+func isFollowing(follower, followee, url string) (bool, error) {
 	// use mobile site b/c desktop site loads followers info via javascript.
-	res, err := http.Get(fmt.Sprintf("https://mobile.twitter.com/%s/following", follower))
+	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
 		return false, err
 	}
 	defer res.Body.Close()
 
-	return findScreenName(res, followee), nil
+	found, nextPageURL := findScreenName(res, followee)
+	if found {
+		return found, nil
+	} else if nextPageURL != "" {
+		url := fmt.Sprintf("https://mobile.twitter.com%s", nextPageURL)
+		return isFollowing(follower, followee, url)
+	} else {
+		return false, nil
+	}
 }
 
-func findScreenName(res *http.Response, screenName string) bool {
+func findScreenName(res *http.Response, screenName string) (bool, string) {
 	z := html.NewTokenizer(res.Body)
-	found := false
+	var found, morePages bool
+	var nextPageURL string
+
+	_ = morePages
+
 	for {
 		tt := z.Next()
 
 		switch {
 		case tt == html.ErrorToken:
 			// End of the document, we're done
-			return found
+			return found, nextPageURL
+		case tt == html.TextToken:
+			t := z.Token()
+			// fmt.Println(t)
+
+			if strings.ToLower(t.Data) == "show more people" {
+				fmt.Println("more pages")
+				morePages = true
+			}
 		case tt == html.StartTagToken:
 			t := z.Token()
 
@@ -59,11 +80,13 @@ func findScreenName(res *http.Response, screenName string) bool {
 				for _, a := range t.Attr {
 					// fmt.Println(a.Key)
 					if a.Key == "href" {
-						// fmt.Println("Found user:", a.Val)
 						//TODO: use ToLowerSpecial() ?
 						if strings.Contains(strings.ToLower(a.Val), strings.ToLower(screenName)) {
 							found = true
 							break
+						}
+						if strings.Contains(strings.ToLower(a.Val), "/following?cursor") {
+							nextPageURL = a.Val
 						}
 					}
 				}
